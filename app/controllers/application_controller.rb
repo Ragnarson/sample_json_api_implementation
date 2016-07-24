9bab4a135bf6570b74b44c16990292b3c7dcbbb2
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2016-07-24 19:27:46 +0200

    Check generic errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::API
+  before_action :set_content_type, :check_accept_header
+
+  rescue_from ActiveRecord::RecordNotFound do
+    render json: {
+      errors: [
+        {
+          status: 404,
+          details: "Not Found"
+        }
+      ]
+    }
+  end
+
+  private
+
+  def set_content_type
+    response.headers["Content-Type"] = "application/vnd.api+json"
+  end
+
+  def check_accept_header
+    unless request.headers["Accept"] == "application/vnd.api+json"
+      return render json: {
+        errors: [
+          {
+            status: 406,
+            details: "Not Acceptable"
+          }
+        ]
+      }
+    end
+  end
 end
