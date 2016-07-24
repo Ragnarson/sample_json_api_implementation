26b0a60bff28f4df2e636050518f59a27ae164b2
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2016-07-24 18:19:40 +0200

    Add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :albums, only: %i(index show)
+  resources :artists, only: %i(index show)
+  resources :songs, only: %i(index show)
 end
